2c583424ae7380557c9fcde7e634a7831cfd78d7
Justine Leleu <justine.leleu.28@gmail.com>
2023-10-25 12:11:42 +0200

    test

--- php/index.php
+++ php/index.php
@@ -2,19 +2,19 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="./dist/style.css" type="text/css">
+    <link rel="stylesheet" href="../dist/style.css" type="text/css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AZ-Store</title>
 </head>
 <body>
 <?php
 
-require 'nav.php';
-require 'section-1.php';
-require 'section-2.php';
-require 'section-3.php';
-require 'section-4.php';
-require 'footer.php';
+require 'partials/nav.php';
+require 'partials/section-1.php';
+require 'partials/section-2.php';
+require 'partials/section-3.php';
+require 'partials/section-4.php';
+require 'partials/footer.php';
 
 ?>
     
---
+++ php/partials/section-3.php
@@ -0,0 +1,5 @@
+<section>
+    <img src="./../asset/img/shoe_two.png" alt="shoe image">
+    <h2>WE PROVIDE YOU THE <span>BEST</span>QUALITY.</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt vel non libero necessitatibus, ex commodi nobis neque perferendis modi, odit nisi adipisci.</p>
+</section>
