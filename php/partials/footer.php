f37c44c246ada334a6d00f076d50f645bccc6282
Venturi <bastien.aristide.venturi@gmail.com>
2023-10-25 13:59:18 +0200

    structure

--- php/partials/footer.php
+++ php/partials/footer.php
@@ -1,8 +1,10 @@
 <footer>
 
-    <span>home</span>
-    <span>about</span>
-    <span>poduct</span>
-    <span>contact</span>
+<ul class="footerContainer">
+        <li><a class="footerItems" href="">Home</a></li>
+        <li><a class="footerItems" href="">About</a></li>
+        <li><a class="footerItems" href="">Products</a></li>
+        <li><a class="footerItems" href="">Contact</a></li>
+    </ul>
 
 </footer>
--- php/partials/section-1.php
+++ php/partials/section-1.php
@@ -1,4 +1,4 @@
-<div class="section-1">
+<section class="section-1">
 
     <img id="shoe" src="/asset/img/shoe_one.png" alt="shoe">
     <h1 id="brand">NIKE</h1>
@@ -8,4 +8,4 @@
         <input type="button" id="storeBtn" value="See our store">
     </div>
     
-</div>
+</section>
