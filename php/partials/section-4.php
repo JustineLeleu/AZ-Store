b7ef02413e6b4f9e91690391f06c2c6926825f46
Mathias Barbier <mathbarbier07@gmail.com>
2023-10-25 12:31:26 +0200

    update

--- php/partials/section-4.php
+++ php/partials/section-4.php
@@ -7,13 +7,13 @@
     </div>
 
     <div class= 'review2'>
-    <img src="../asset/img/image-thomas.png" alt="thomas">
+    <img src="../asset/img/image-thomas.jpg" alt="thomas">
     <h3>thomas from corporate</h3>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium praesentium ipsa molestiae modi, nesciunt officia distinctio officiis nulla repellendus vitae consequatur numquam!</p>
     </div>
 
     <div class= 'review3'>
-    <img src="../asset/img/image-jennie.png" alt="jennie">
+    <img src="../asset/img/image-jennie.jpg" alt="jennie">
     <h3>jennie from Nike</h3>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium praesentium ipsa molestiae modi, nesciunt officia distinctio officiis nulla repellendus vitae consequatur numquam!</p>
     </div>
