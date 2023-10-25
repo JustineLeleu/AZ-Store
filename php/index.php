137d78b04b186c3d5fa64a8485111f7cb8060a7f
Mathias Barbier <mathbarbier07@gmail.com>
2023-10-25 10:27:56 +0200

    update index.php

---
+++ php/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./dist/style.css" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AZ-Store</title>
+</head>
+<body>
+<?php
+
+require 'nav.php';
+require 'section-1.php';
+require 'section-2.php';
+require 'section-3.php';
+require 'section-4.php';
+require 'footer.php';
+
+?>
+    
+</body>
+</html>
+
