410027637414b3ad8b692419f8619426f61a0d3f
Venturi <bastien.aristide.venturi@gmail.com>
2023-10-25 12:24:36 +0200

    midi

--- php/partials/section-4.php
+++ php/partials/section-4.php
@@ -1,19 +1,18 @@
-<section>
 
-    <div class= 'review1'>
-    <img src="../asset/img/image-emily.jpg" alt="emily">
+<div class= 'review1'>
+    <img src="./asset/img/image-emily.jpg" alt="emily">
     <h3>emily from xyz</h3>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium praesentium ipsa molestiae modi, nesciunt officia distinctio officiis nulla repellendus vitae consequatur numquam!</p>
     </div>
 
     <div class= 'review2'>
-    <img src="../asset/img/image-thomas.png" alt="thomas">
+    <img src="./asset/img/image-thomas.png" alt="thomas">
     <h3>thomas from corporate</h3>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium praesentium ipsa molestiae modi, nesciunt officia distinctio officiis nulla repellendus vitae consequatur numquam!</p>
     </div>
 
     <div class= 'review3'>
-    <img src="../asset/img/image-jennie.png" alt="jennie">
+    <img src="./asset/img/image-jennie.png" alt="jennie">
     <h3>jennie from Nike</h3>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium praesentium ipsa molestiae modi, nesciunt officia distinctio officiis nulla repellendus vitae consequatur numquam!</p>
     </div>
