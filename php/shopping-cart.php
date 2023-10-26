a94ce51bf180b29d9730189b2d0509d5a210ae75
Justine Leleu <justine.leleu.28@gmail.com>
2023-10-26 15:57:45 +0200

    add delete all items button

--- php/shopping-cart.php
+++ php/shopping-cart.php
@@ -2,21 +2,20 @@
 
 if (isset($_POST['addItem']))
 {
-    //$item = array_search($_POST["addItem"], array_column($_SESSION['shoppingCart'], 'id'));
     $_SESSION['shoppingCart'][$_POST['addItem']]['count']++;
-    //echo "<script>console.log($item);</script>";
-    //echo "<script>console.log('test');</script>";
 }
 if (isset($_POST['removeItem']))
 {
-    $item = array_search($_POST["removeItem"], array_column($_SESSION['shoppingCart'], 'id'));
     $_SESSION['shoppingCart'][$_POST['removeItem']]['count']--;
-    echo "<script>console.log($item);</script>";
 
     if ($_SESSION['shoppingCart'][$_POST['removeItem']]['count'] == 0)
     {
         unset($_SESSION['shoppingCart'][$_POST['removeItem']]);
     }
+}
+if (isset($_POST['deleteAll']))
+{
+    unset($_SESSION['shoppingCart']);
 }
 
 $itemsCount = 0;
@@ -32,32 +31,36 @@ foreach ($_SESSION['shoppingCart']  as $value)
 
 <div id="shopping-cart" class="w-3/4 h-full fixed right-0 top-0 bg-gray-200 text-black hidden">
     <button id="closeCartButton" class="fixed w-10"><img src="./../asset/img/arrow.png" alt="arrow"></button>
+    <form method="post">
+        <button name="deleteAll" class="fixed right-0 w-6"><img src="./../asset/img/delete.png" alt="delete image"></button>
+    </form>
     <h2 class="text-center">Shopping cart</h2>
 
     <div class="flex flex-col items-center gap-0.5">
         <h3><?php echo $itemsCount." items"; ?></h3>
 
         <?php
-        
-        foreach ($_SESSION['shoppingCart'] as $value) 
+        if (isset($_SESSION['shoppingCart']))
         {
-            ?>
-
-            <div class="flex justify-around w-full items-center bg-white py-3">
-                <img src="<?php echo $value['img']; ?>" alt="" class="w-20">
-                <?php echo $value['name']; ?>
-                <h4><?php echo $value['name']; ?></h4>
-                <div class="flex items-center">
-                    <div><?php echo $value['count']; ?></div>
-                    <form method="post" class="flex flex-col">
-                        <button name="addItem" value="<?php echo $value['id']; ?>"><img src="./../asset/img/arrow-up.png" alt="arrow" class="w-4"></button>
-                        <button name="removeItem" value="<?php echo $value['id']; ?>"><img src="./../asset/img/arrow-down.png" alt="arrow" class="w-4"></button>
-                    </form>
+            foreach ($_SESSION['shoppingCart'] as $value) 
+            {
+                ?>
+    
+                <div class="flex justify-around w-full items-center bg-white py-3">
+                    <img src="<?php echo $value['img']; ?>" alt="" class="w-20">
+                    <h4><?php echo $value['name']; ?></h4>
+                    <div class="flex items-center">
+                        <div><?php echo $value['count']; ?></div>
+                        <form method="post" class="flex flex-col">
+                            <button name="addItem" value="<?php echo $value['id']; ?>"><img src="./../asset/img/arrow-up.png" alt="arrow" class="w-4"></button>
+                            <button name="removeItem" value="<?php echo $value['id']; ?>"><img src="./../asset/img/arrow-down.png" alt="arrow" class="w-4"></button>
+                        </form>
+                    </div>
+                    <div><?php echo $value['price']."€"; ?></div>
                 </div>
-                <div><?php echo $value['price']."€"; ?></div>
-            </div>
-
-            <?php
+    
+                <?php
+            }   
         }
 
         ?>
