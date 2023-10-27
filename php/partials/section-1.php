d682b4457f0e807a9238f3352d2b48f14fd55803
Venturi <bastien.aristide.venturi@gmail.com>
2023-10-27 15:50:06 +0200

    fin css?

--- php/partials/section-1.php
+++ php/partials/section-1.php
@@ -1,12 +1,12 @@
-<section class="section-1 flex flex-row items-center justify-center h-80 mx-20 mb-40 ">
+<section class="section-1 flex flex-row items-center justify-around h-80 mx-20 mb-40 ">
 
-    <div class="descContainer  p-20 mt-20">
+    <div class="descContainer  p-60 mt-20">
         <h1 id="description" class='font-titleFont text-5xl '>SHOE THE RIGHT ONE.</h1>
         <input type="button" id="storeBtn" value="See our store" class="bg-blue-500  px-4 py-2 mt-2 rounded-lg font-btn">
     </div>
 
     <div class="nike relative h-60 m-20">
-        <img class="absolute z-2  object-left-top h-50" id="shoe" src="/asset/img/shoe_one.png" alt="shoe">
+        <img class="absolute z-2  object-left-top" id="shoe" src="/asset/img/shoe_one.png" alt="shoe">
         <img class="m-10" id="nike" src="/asset/img/Nike.png" alt="Nike">
             </h1>
     </div>
